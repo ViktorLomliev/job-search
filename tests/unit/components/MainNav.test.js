7b61a0052f13c571c47f2ae9897063f765e2255d
vlomliev <viktor.lomliev@dxc.com>
2024-02-23 21:51:33 +0200

    add: nav items test

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,7 +6,20 @@ describe('MainNav', () => {
   it('displays company name', () => {
     render(MainNav)
     const companyName = screen.getByText('JobHive')
-    console.log(companyName)
     expect(companyName).toBeInTheDocument()
+  })
+
+  it('displays menu items for navigation', () => {
+    render(MainNav)
+    const navigationMenuItems = screen.getAllByRole('listitem')
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.textContent)
+    expect(navigationMenuTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at JobHive',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
   })
 })
