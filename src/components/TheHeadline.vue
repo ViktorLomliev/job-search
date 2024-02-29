0356fcf9d1edd8d87bf26c40919e28c504ea271d
vlomliev <viktor.lomliev@dxc.com>
2024-02-29 15:21:15 +0200

    add: change verb and text color on interval

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -3,18 +3,63 @@ export default {
   name: 'TheHeadline',
   data() {
     return {
-      sample: 'Hello'
+      action: 'Build',
+      interval: null
+    }
+  },
+  computed: {
+    actionClasses() {
+      return {
+        build: this.action === 'Build',
+        create: this.action === 'Create',
+        design: this.action === 'Design',
+        code: this.action === 'Code'
+      }
     }
   },
   created() {
-    console.log('Ive been created: My data :', this.sample)
+    this.changeTitle()
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ['Build', 'Create', 'Design', 'Code']
+        const currentActionIndex = actions.indexOf(this.action)
+        const nextActionIndex = (currentActionIndex + 1) % 4
+        const nextAction = actions[nextActionIndex]
+        this.action = nextAction
+      }, 3000)
+    }
   }
 }
 </script>
 
 <template>
   <section>
-    <h1>Build for everyone</h1>
+    <h1>
+      <span :class="actionClasses">{{ action }}</span> <br />
+      for everyone
+    </h1>
     <h2>Find your nect job at JobHive</h2>
   </section>
 </template>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+
+.create {
+  color: #34a853;
+}
+.design {
+  color: #f9ab00;
+}
+
+.code {
+  color: #d93025;
+}
+</style>
