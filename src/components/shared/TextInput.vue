d67cf65682846a73bf536e699f6692dfc181fd9d
vlomliev <viktor.lomliev@dxc.com>
2024-03-01 15:11:40 +0200

    add: create text input component

---
+++ src/components/shared/TextInput.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'TextInput',
+  data() {
+    return {
+      value: ''
+    }
+  },
+  methoda: {
+    handleInput($event) {
+      this.value = $event.target.value
+    }
+  }
+}
+</script>
+
+<template>
+  <input
+    type="text"
+    :value="value"
+    class="w-full text-lg font-normal focus:outline-none"
+    @input="handleInput"
+  />
+</template>
