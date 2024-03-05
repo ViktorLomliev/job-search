7c1b7ce83bbc677aaadb48ec8ed1c8131d956fe0
vlomliev <viktor.lomliev@dxc.com>
2024-03-05 11:26:31 +0200

    add: query params to job results

--- src/components/job_search/JobSearchForm.vue
+++ src/components/job_search/JobSearchForm.vue
@@ -5,11 +5,22 @@ import TextInput from '../shared/TextInput.vue'
 
 export default {
   name: 'JobSearchForm',
-  components: { FontAwesomeIcon, ActionButton, TextInput, TextInput },
+  components: { FontAwesomeIcon, ActionButton, TextInput },
   data() {
     return {
       role: '',
       location: ''
+    }
+  },
+  methods: {
+    searchForJobs() {
+      this.$router.push({
+        name: 'JobResults',
+        query: {
+          role: this.role,
+          location: this.location
+        }
+      })
     }
   }
 }
@@ -17,22 +28,24 @@ export default {
 
 <template>
   <form
-    class="border-bramd border-brand-gray-3 flex h-12 w-full items-center rounded-3xl border border-solid"
+    class="border-bramd flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3"
+    @submit.prevent="searchForJobs"
   >
     <FontAwesomeIcon :icon="['fas', 'search']" class="ml-4 mr-3" />
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
-        <label class="absolute -top-10 left-0">Role</label>
-        <TextInput v-model="role" placeholder="Software engineer" />
+        <label for="role" class="absolute -top-10 left-0">Role</label>
+        <TextInput id="role" v-model="role" placeholder="Software engineer" />
       </div>
       <span
-        class="border-brand-gray-3 bg-brand-gray-2 flex h-full items-center border-l border-r px-3"
-        >in</span
+        class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
       >
+        in
+      </span>
       <div class="relative flex h-full flex-1 items-center pl-3">
-        <label class="absolute -top-10 left-0">Where?</label>
+        <label for="location" class="absolute -top-10 left-0">Where?</label>
 
-        <TextInput v-model="location" placeholder="Sofia" />
+        <TextInput id="location" v-model="location" placeholder="Sofia" />
       </div>
     </div>
     <ActionButton text="Search" type="secondary" class="rounded-r-3xl" />
