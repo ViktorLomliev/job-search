cd63fe8739a895ae940507ef2037452d416d3499
vlomliev <viktor.lomliev@dxc.com>
2024-03-06 11:38:49 +0200

    refactor: JobListings tests to use factory factory functions and test the dynamic pagination based on query

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -6,20 +6,34 @@ import { RouterLinkStub } from '@vue/test-utils'
 vi.mock('axios')
 
 describe('JobListings', () => {
-  it('fetches jobs', () => {
-    axios.get.mockResolvedValue({ data: [] })
-    render(JobListings)
-    expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs')
+  const createRoute = (queryParams = {}) => ({
+    query: {
+      page: '5',
+      ...queryParams
+    }
   })
-  it('displayes maximum of 10 jobs', async () => {
-    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+  const renderJobListings = ($route) => {
     render(JobListings, {
       global: {
+        mocks: {
+          $route
+        },
         stubs: {
           RouterLink: RouterLinkStub
         }
       }
     })
+  }
+  it('fetches jobs', () => {
+    axios.get.mockResolvedValue({ data: [] })
+    const $route = createRoute()
+    renderJobListings($route)
+    expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/jobs')
+  })
+  it('displays maximum of 10 jobs', async () => {
+    axios.get.mockResolvedValue({ data: Array(15).fill({}) })
+    const $route = createRoute({ page: '1' })
+    renderJobListings($route)
     const jobListings = await screen.findAllByRole('listitem')
     expect(jobListings).toHaveLength(10)
   })
