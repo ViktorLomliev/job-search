7d616b3d5b9a341c2b3c2297220b5e0bcbac3a72
vlomliev <viktor.lomliev@dxc.com>
2024-03-07 21:25:01 +0200

    add: organization checkboxes as a slots in CollapsibleAccordion.vue

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -7,7 +7,33 @@
           <ActionButton text="Clear Filters" type="secondary" />
         </div>
       </div>
-      <CollapsibleAccordion />
+      <CollapsibleAccordion header="Job types">
+        <p>Placeholder</p>
+      </CollapsibleAccordion>
+      <CollapsibleAccordion header="Organizations">
+        <div class="mt-5">
+          <fieldset>
+            <ul class="flex flex-row flex-wrap">
+              <li class="h-6 w-1/2">
+                <input id="VueTube" type="checkbox" class="mr-3" />
+                <label for="VueTube">VueTube</label>
+              </li>
+              <li class="h-6 w-1/2">
+                <input id="Between Vue an Me" type="checkbox" class="mr-3" />
+                <label for="Between Vue an Me">Between Vue an Me</label>
+              </li>
+              <li class="h-6 w-1/2">
+                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
+                <label for="Et Vue Brute">Et Vue Brute</label>
+              </li>
+              <li class="h-6 w-1/2">
+                <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
+                <label for="Vue and a Half Men">Vue and a Half Men</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+      </CollapsibleAccordion>
     </section>
   </div>
 </template>
