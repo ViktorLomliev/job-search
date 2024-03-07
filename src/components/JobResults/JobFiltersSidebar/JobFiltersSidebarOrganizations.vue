3f606fbe67f4355579f6d84b8b863b5bc14140e0
vlomliev <viktor.lomliev@dxc.com>
2024-03-08 00:55:16 +0200

    add: extract organizations accordion to new component

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,37 @@
+<script>
+import CollapsibleAccordion from '@/components/shared/CollapsibleAccordion.vue'
+
+export default {
+  name: 'JobFiltersSidebarOrganizations',
+  components: {
+    CollapsibleAccordion
+  }
+}
+</script>
+
+<template>
+  <CollapsibleAccordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li class="h-6 w-1/2">
+            <input id="VueTube" type="checkbox" class="mr-3" />
+            <label for="VueTube">VueTube</label>
+          </li>
+          <li class="h-6 w-1/2">
+            <input id="Between Vue an Me" type="checkbox" class="mr-3" />
+            <label for="Between Vue an Me">Between Vue an Me</label>
+          </li>
+          <li class="h-6 w-1/2">
+            <input id="Et Vue Brute" type="checkbox" class="mr-3" />
+            <label for="Et Vue Brute">Et Vue Brute</label>
+          </li>
+          <li class="h-6 w-1/2">
+            <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
+            <label for="Vue and a Half Men">Vue and a Half Men</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </CollapsibleAccordion>
+</template>
