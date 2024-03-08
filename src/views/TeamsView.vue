191584bc6e9b2ff180b814fabf4381568f49ba25
vlomliev <viktor.lomliev@dxc.com>
2024-03-08 11:20:45 +0200

    add: HeaderContainer tests

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,15 +1,16 @@
 <template>
   <HeaderContainer>
-    <template v-slot:title>
+    <template #title>
       <h1 class="w-full text-4xl font-normal">Teams</h1>
     </template>
-    <template v-slot:subtitle>
+    <template #subtitle>
       <h2 class="my-4 w-full text-base font-light">
         It's awesome working here. Why don't you come join
       </h2>
     </template>
   </HeaderContainer>
 </template>
+
 <script>
 import HeaderContainer from '@/components/shared/HeaderContainer.vue'
 export default {
