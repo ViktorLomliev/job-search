ba98b8dc6d82fe2d1dc01b26e95cbd98c73e0fe9
vlomliev <viktor.lomliev@dxc.com>
2024-03-08 11:09:41 +0200

    add: content to teams slots

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,5 +1,14 @@
 <template>
-  <HeaderContainer />
+  <HeaderContainer>
+    <template v-slot:title>
+      <h1 class="w-full text-4xl font-normal">Teams</h1>
+    </template>
+    <template v-slot:subtitle>
+      <h2 class="my-4 w-full text-base font-light">
+        It's awesome working here. Why don't you come join
+      </h2>
+    </template>
+  </HeaderContainer>
 </template>
 <script>
 import HeaderContainer from '@/components/shared/HeaderContainer.vue'
