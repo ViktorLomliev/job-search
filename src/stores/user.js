945c552222344981384febb94643e0d04a23c22d
vlomliev <viktor.lomliev@dxc.com>
2024-03-12 15:16:23 +0200

    add: ADD_SELECTED_JOB_TYPES getter

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,11 +1,13 @@
 import { defineStore } from 'pinia'
 
 export const ADD_SELECTED_ORGANIZATION = 'ADD_SELECTED_ORGANIZATION'
+export const ADD_SELECTED_JOB_TYPES = 'ADD_SELECTED_JOB_TYPES'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
     isLoggedIn: false,
-    selectedOrganizations: []
+    selectedOrganizations: [],
+    selectedJobTypes: []
   }),
   actions: {
     loginUser() {
@@ -13,6 +15,9 @@ export const useUserStore = defineStore('user', {
     },
     [ADD_SELECTED_ORGANIZATION](organizations) {
       this.selectedOrganizations = organizations
+    },
+    [ADD_SELECTED_JOB_TYPES](jobTypes) {
+      this.selectedJobTypes = jobTypes
     }
   }
 })
