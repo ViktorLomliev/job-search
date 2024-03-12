e4542ac28302233872d289f37ee1fce1c81778f6
vlomliev <viktor.lomliev@dxc.com>
2024-03-12 15:17:14 +0200

    add: tests for UNIQUE_JOB_TYPES getter

--- tests/unit/stores/jobs.test.js
+++ tests/unit/stores/jobs.test.js
@@ -48,6 +48,15 @@ describe('getter', () => {
       expect(result).toEqual(new Set(['Google', 'Amazon']))
     })
   })
+  describe('UNIQUE_JOB_TYPES', () => {
+    it('finds unique job types from list of jobs', () => {
+      const store = useJobsStore()
+      store.jobs = [{ jobType: 'Full-time' }, { jobType: 'Temporary' }, { jobType: 'Full-time' }]
+
+      const result = store.UNIQUE_JOB_TYPES
+      expect(result).toEqual(new Set(['Full-time', 'Temporary']))
+    })
+  })
   describe('FILTER_JOBS_BY_ORGANIZATIONS', () => {
     it('identifies jobs that are associated with the given organizations', () => {
       const jobsStore = useJobsStore()
