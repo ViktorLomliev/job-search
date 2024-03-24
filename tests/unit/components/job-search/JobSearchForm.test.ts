58ad0b36aae05833854808c7467c497b56b0e1fd
vlomliev <viktor.lomliev@dxc.com>
2024-03-24 18:44:40 +0200

    refactor: JobSearchForm from js to ts and fix the tests

--- tests/unit/components/job-search/JobSearchForm.test.ts
+++ tests/unit/components/job-search/JobSearchForm.test.ts
@@ -1,14 +1,17 @@
+import type { Mock } from 'vitest'
 import { render, screen } from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
 import JobSearchForm from '@/components/job_search/JobSearchForm.vue'
 import { useRouter } from 'vue-router'
 vi.mock('vue-router')
 
+const useRouterMock = useRouter as Mock
+
 describe('JobSearchForm', () => {
   describe('when user submits form', () => {
     it('directs user to job results page with users search parameters', async () => {
       const push = vi.fn()
-      useRouter.mockReturnValue({ push })
+      useRouterMock.mockReturnValue({ push })
       render(JobSearchForm, {
         global: {
           stubs: {
