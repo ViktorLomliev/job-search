cc88db972178d52fb10da97b4dc113259b8f47ee
vlomliev <viktor.lomliev@dxc.com>
2024-03-25 10:10:43 +0200

    add: clear filters action

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia'
 export const ADD_SELECTED_ORGANIZATION = 'ADD_SELECTED_ORGANIZATION'
 export const ADD_SELECTED_JOB_TYPES = 'ADD_SELECTED_JOB_TYPES'
 export const ADD_SELECTED_DEGREES = 'ADD_SELECTED_DEGREES'
+export const CLEAR_USER_JOB_FILTER_SELECTION = 'CLEAR_USER_JOB_FILTER_SELECTION'
 
 export interface UserState {
   isLoggedIn: boolean
@@ -30,6 +31,11 @@ export const useUserStore = defineStore('user', {
     },
     [ADD_SELECTED_DEGREES](degrees: string[]) {
       this.selectedDegrees = degrees
+    },
+    [CLEAR_USER_JOB_FILTER_SELECTION]() {
+      this.selectedDegrees = []
+      this.selectedJobTypes = []
+      this.selectedOrganizations = []
     }
   }
 })
