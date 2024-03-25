44d956cd75d324827376e448be6baec3f0ea8886
vlomliev <viktor.lomliev@dxc.com>
2024-03-25 11:31:14 +0200

    add: JobFiltersSidebarPrompt component

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -1,31 +1,15 @@
 <script lang="ts" setup>
-import ActionButton from '@/components/shared/ActionButton.vue'
-import { useUserStore } from '@/stores/user'
 import CollapsibleAccordion from '@/components/shared/CollapsibleAccordion.vue'
 import JobFiltersSidebarDegrees from './JobFiltersSidebarDegrees.vue'
 import JobFiltersSidebarJobTypes from './JobFiltersSidebarJobTypes.vue'
 import JobFiltersSidebarOrganizations from './JobFiltersSidebarOrganizations.vue'
-
-const jobsStore = useJobsStore()
-
-const degreesStore = useDegreesStore()
-
-const userStore = useUserStore()
+import JobFiltersSidebarPrompt from './JobFiltersSidebarPrompt.vue'
 </script>
 
 <template>
   <div class="flex w-96 flex-col border-r border-solid border-brand-gray-1 bg-white p-4">
     <section class="pb-5">
-      <div class="f;ex-rpw flex justify-between">
-        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
-        <div class="flex items-center text-sm">
-          <ActionButton
-            text="Clear Filters"
-            type="secondary"
-            @click="userStore.CLEAR_USER_JOB_FILTER_SELECTION"
-          />
-        </div>
-      </div>
+      <JobFiltersSidebarPrompt />
       <CollapsibleAccordion header="Degrees">
         <JobFiltersSidebarDegrees />
       </CollapsibleAccordion>
---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarPrompt.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import ActionButton from '@/components/shared/ActionButton.vue'
+import { useUserStore } from '@/stores/user'
+const userStore = useUserStore()
+</script>
+
+<template>
+  <div class="f;ex-rpw flex justify-between">
+    <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+    <div class="flex items-center text-sm">
+      <ActionButton
+        text="Clear Filters"
+        type="secondary"
+        @click="userStore.CLEAR_USER_JOB_FILTER_SELECTION"
+      />
+    </div>
+  </div>
+</template>
